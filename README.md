48b02dc01afb457a32dccf24a454786dc357bbf7
anserwaseem <5922malikanser@gmail.com>
2020-06-12 17:08:17 +0500

    no proper exit but it does it work

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # check-relitive-primerity
+
+Harry wants to check the relative primerity of 2 numbers. For this purpose, he checks the GCD (Greatest
+Common Divisor) of the numbers. If GCD comes out 1 then numbers are relative prime to each other.
+Harry’s requirements are as follows:
+ Subroutine DEC_IN should load two registers (BX and DX) with two numbers. Numbers should be a
+2 - digit decimal ranging from (01 - 99).
+ Subroutine GCD_AB apply the logic for GCD of two numbers. GCD of two numbers is performed as
+follows:
+1) Given two numbers, one in bx and other in dx
+2) If bx=dx, GCD is bx, move to step 5
+3) In case bx&amp;lt;dx, swap both so that bx has greater value and dx has smaller one.
+4) Repeat until dx is not zero:
+a) bx = bx mod dx
+b) swap values of bx and dx
+5) If bx=1, then GCD is 1 and the numbers provided in DEC_IN are relatively prime.
+ Subroutine DEC_OUT should display the GCD and print whether the numbers were relatively prime
+or not.
--- crp.asm
+++ crp.asm
@@ -1,38 +1,37 @@
 [org 0x100]
 jmp start
 
+GCD_message: db	10, 13, '  GCD = $'
+GCD:         db	10, 13, '  $'
 prime:       db	10, 13, 'Numbers are relatively prime $'
 notPrime:    db	10, 13, 'Numbers are not relatively prime $' 
 
 DEC_IN:
+        push bp
+        mov bp, sp
+        pusha
 
-push bp
-mov bp, sp
-pusha
+        mov bx, [bp+6];1st number
+        mov dx, [bp+4];2nd number
 
-mov bx, [bp+6];1st number
-mov dx, [bp+4];2nd number
+        call GCD_AB
 
-call GCD_AB
-
-popa
-ret 4
+        popa
+        pop bp
+        ret 4
 
 GCD_AB:
+        cmp dx, bx
+        je eq
 
-;mov ax, 0; 0 means numbers are not relatively prime
-
-cmp dx, bx
-je eq
-
-cmp dx, bx
-ja swap
+        cmp dx, bx
+        ja swap
+        jmp l
 
 
-
-eq:     ;mov ax, 1
-        push bx
+eq:     push bx
         call DEC_OUT
+        jmp exitGCD_AB
 
 ; swapping bx and dx so that bx has greater value and dx has smaller one.
 swap:   mov cx, bx
@@ -40,45 +39,66 @@ swap:   mov cx, bx
         mov dx, cx
 
 ; doing  bx = bx mod dx  untill dx = 0
-l:      ;push ax
-        mov ax, bx ; number to divide goes in AX
-        mov bx, dx ; dividing-by (operand)
-        div bx     ; perform division. <- WORD division ; Stores integer result in AL and remainder in DX
-;swapping bx and dx
-        ; mov cx, dx 
-        ; mov dx, bx
-        ; mov bx, cx
+l:      mov ax, bx      ; dividend in AX;20
+        mov bx, dx      ; divisor in BX;9
+        xor dx, dx      ; remainder
+        div bx          ; perform division. <- WORD division ; Stores integer result in AL and remainder in DX
 
         cmp dx, 0x00
         jne l
 
         push bx
         call DEC_OUT
-ret
+
+exitGCD_AB:     ret
+
 
 DEC_OUT:
         cmp bx, 1
         jne displayNotPrime
 
+; clearing the screen
+        mov ax, 0xb800 		; load video base in ax
+		mov es, ax 			; point es to video base
+		xor di, di 			; point di to top left column
+        mov ax, 0x0720      ; space char in normal attribute
+        mov cx, 2000        ; number of screen locations
+        cld                 ; auto increment mode
+        rep stosw           ; clear the whole screen
+
+; printing GCD =
+        mov  dx, GCD_message     
+        mov  ah, 9          ; service 9 – write string               
+        int  0x21           ; dos services 
+
+; printing the value of GCD        
+        add bx, 30h         ; adding 30h in ascii
+        mov si, GCD         ; pointing si to GCD string
+        mov [si], bl        ; inserting value of bl at the start of GCD string
+        mov  dl, GCD        
+        mov  ah, 9          ; service 9 – write string               
+        int  0x21           ; dos services 
+; printing Numbers are relatively prime
         mov  dx, prime      
-        mov  ah, 9              ; service 9 – write string               
-        int  0x21               ; dos services 
+        mov  ah, 9          ; service 9 – write string               
+        int  0x21           ; dos services 
+
+        jmp exitDEC_OUT
 
 displayNotPrime:
-        mov  dx, notPrime     
-        mov  ah, 9              ; service 9 – write string               
-        int  0x21               ; dos services 
-ret
+; printing Numbers are not relatively prime
+        mov  dx, notPrime       
+        mov  ah, 9          ; service 9 – write string               
+        int  0x21           ; dos services 
+
+exitDEC_OUT:    ret
+
 
 start:
 
-push 20;1st number
-push 9;2nd number
+push 12;1st number
+push 93;2nd number
 call DEC_IN
 
-;23
-;45
-;DEC_IN call
-;bp
-;GCD_AB call
-;
+mov ax, 0x4c00 ; terminate program
+int 0x21
